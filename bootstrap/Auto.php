7a6e82410d7fd07f36cce86a18ef6d133bdbc252
eXeCUT <sexecut@gmail.com>
2020-08-09 18:53:01 +0300

    added auto bootstrapper

---
+++ bootstrap/Auto.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ */
+
+namespace execut\cms\bootstrap;
+
+
+use yii\base\BootstrapInterface;
+use yii\console\Application;
+
+class Auto implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        echo 1;
+        exit;
+        $bootstraps = [];
+        if ($app instanceof Application) {
+            $bootstraps[] = new Console();
+        } else if ($app instanceof \yii\web\Application) {
+            if ($app->id === 'app-backend') {
+                $bootstraps[] = new Backend();
+            } else if ($app->id === 'app-frontend') {
+                $bootstraps[] = new Frontend();
+            } else {
+                $bootstraps[] = new Frontend();
+                $bootstraps[] = new Backend();
+            }
+        }
+
+        foreach ($bootstraps as $bootstrap) {
+            $bootstrap->bootstrap($app);
+        }
+    }
+}
